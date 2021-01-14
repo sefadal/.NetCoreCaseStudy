5be64629cd44aa64cb8268a380f51a2f5fa11b3e
MAVI\melihdal <melih.dal@mavi.com>
2021-01-14 19:46:04 +0300

    cv

--- WepApi/Startup.cs
+++ WepApi/Startup.cs
@@ -1,23 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using WepApi.Helpers;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 using WebApi.Services;
+using WepApi.Helpers;
 
 namespace WepApi
 {
